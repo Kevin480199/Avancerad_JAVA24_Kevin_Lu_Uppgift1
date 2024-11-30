a9635a9b802debb42942ae193aca9bbe1668d737
Kevin Lu <ke28lu@gmail.com>
2024-11-30 09:21:32 +0100

    Updated all subclasses with constructor and methods

--- src/main/java/org/example/SUV.java
+++ src/main/java/org/example/SUV.java
@@ -1,7 +1,16 @@
 package org.example;
 
 public class SUV extends Vehicle {
-    public SUV() {
+    private int year;
+    public SUV(String model, String regNumber, double price, int year) {
+        super(model, regNumber, price);
+        this.year = year;
 
+    }
+    public int getYear() {
+        return year;
+    }
+    public void ligthsOn(){
+        System.out.println("Ligths on");
     }
 }
--- src/main/java/org/example/Truck.java
+++ src/main/java/org/example/Truck.java
@@ -1,7 +1,16 @@
 package org.example;
 
 public class Truck extends Vehicle {
-    public Truck() {
+    private int doors;
+    public Truck(String model, String regNumber, double price, int doors) {
+        super(model, regNumber, price);
+        this.doors = doors;
 
+    }
+    public int getDoors() {
+        return doors;
+    }
+    public void lockCar(){
+        System.out.println("Car locked");
     }
 }
